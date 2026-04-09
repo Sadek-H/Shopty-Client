b584b981d4f5df7eee745a4df99c01172b5800cd
Sadek-H <sadek111st@gmail.com>
2026-04-09 17:28:23 +0600

    manage user

--- src/Pages/Dashboard/Admin/Manageusers.jsx
+++ src/Pages/Dashboard/Admin/Manageusers.jsx
@@ -1,4 +1,4 @@
-import React, { use } from 'react';
+
 import { useLoaderData } from 'react-router';
 
 const Manageusers = () => {
@@ -6,7 +6,31 @@ const Manageusers = () => {
     console.log(users);
     return (
         <div>
-            
+            {
+                users.map(user => (
+
+                    <div key={user.id} className='border-2 border-gray-300 p-4 m-4 rounded-lg'>
+                        
+                       <table className='table table-zebra'>
+                        <thead>
+                             <tr>
+                        <th> Name: </th>
+                        <th> Email: </th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        <tr>
+                        <td >{user?.name}</td>
+                        <td >{user?.email}</td>
+</tr>
+                        </tbody>
+                       </table>
+                    </div>
+
+
+                ))
+            }
         </div>
     );
 };
